887bdcd4058cbbba38f39226f69ab8ee3ca4c8ee
noothanpanthayi <noothankrishnan@outlook.com>
2023-09-04 21:44:25 -0500

    all updates

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,6 +11,8 @@ const Header = () => {
   return (
     <header>
         <nav>
+     <NavLink  to="/dashboard" className="active">Home</NavLink>
+
           <NavLink to="/shopping"  className="active">Shopping</NavLink>
           <NavLink to="/world"  className="active">WorldInfo</NavLink>
           <NavLink to="/cart"  className="active">Cart<span className={styles.bold}>({cartlist.length})</span></NavLink>
