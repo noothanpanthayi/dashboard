891700b6986bc878f73d5b3050c2a3b6f8e0ed61
noothanpanthayi <noothankrishnan@outlook.com>
2023-09-06 10:27:05 -0500

    all changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './home.module.css'
 import ShoppingAbout from '../Shopping/About'
 import WorldAbout from '../World/About'
+import ToDoAbout from '../ToDo/About';
 
 const Home = () => {
   return (
@@ -9,8 +10,9 @@ const Home = () => {
       <div>Online Shopping</div>
       <ShoppingAbout />
       <WorldAbout />
+      <ToDoAbout/>
       <div className={header}>Article in Medium</div>
-      <a style={{fontWeight:'bold', color:'blue'}} rel="noreferrer" 
+      <a  className={externalLink} rel="noreferrer" 
       target="_blank" 
       href="https://medium.com/@noothankrishnan_88771/reactjs-patterns-for-delightful-dx-68a7ec795b70">ReactJs Patterns for Delightful DX</a>
 </div>
@@ -19,7 +21,7 @@ const Home = () => {
 
 }
 
-const {container, header}=styles;
+const {container, header, externalLink}=styles;
 
 
 export default Home
