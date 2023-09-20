1a2790d4be001ff69593f7d30420e94f39e830bd
noothanpanthayi <noothankrishnan@outlook.com>
2023-09-20 16:33:01 -0500

    updates

--- src/pages/Shopping/shopping.test.tsx
+++ src/pages/Shopping/shopping.test.tsx
@@ -5,11 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { data } from './utils/mockdata'
 import { mockhandler } from './utils/mockServer'
 
-
-
 describe('Shopping Test Suite ', () => {
-
-    mockhandler('https://dummyjson.com/products', data);
+  mockhandler('https://dummyjson.com/products', data)
 
   it('should render the products on load of the page ', async () => {
     renderWithProviders(
@@ -20,13 +17,15 @@ describe('Shopping Test Suite ', () => {
 
     await waitFor(() => {
       expect(screen.getByText('Galaxy A712')).toBeInTheDocument()
+    // eslint-disable-next-line
       expect(data.products.length).toBe(2)
       const buttons = screen.getAllByRole('button')
+    // eslint-disable-next-line
       expect(buttons).toHaveLength(2)
+    // eslint-disable-next-line
       fireEvent.click(buttons[0])
+    // eslint-disable-next-line
       expect(window.location.pathname).toBe('/cart')
     })
   })
-
-  
 })
