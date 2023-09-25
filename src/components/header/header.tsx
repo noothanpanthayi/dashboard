d7f582eef797f2791801fdb11907e01bc0daff99
noothanpanthayi <noothankrishnan@outlook.com>
2023-09-25 11:57:03 -0500

    all updates

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,178 @@
-import styles from './header.module.css';
-import {NavLink } from 'react-router-dom';
-import './navlink.css';
-import { useSelector } from 'react-redux';
+import styles from './header.module.css'
+import { NavLink } from 'react-router-dom'
+import './navlink.css'
+import { Fragment, useState } from 'react'
 
 const Header = () => {
-  // const {cartlist}:any=useSelector<any>(state=>state.cartlist);
-  
-  const {yellow}=styles;
-  
+  const [menuOpen, setMenuOpen]=useState(false);
+
+  const toggleMenu=()=>{
+
+    setMenuOpen(!menuOpen);
+
+  }
+
+  const hideMenu=()=>{
+    setMenuOpen(false);
+  }
+
   return (
-    <header>
-        <nav style={{display:'flex', justifyContent:'space-between'}}>
-          <div>
+    <Fragment>
+      <header>
+     
+        <div onClick={toggleMenu} className={hamburger}>☰</div>
+        <div className={mainNav}>
+     <nav>
+          <div className={myNav}>
+            <NavLink
+              to="/dashboard"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Dashboard
+            </NavLink>
+            <NavLink
+              to="/grid"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Custom Grid
+            </NavLink>
+            <NavLink
+              to="/world"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              WorldInfo
+            </NavLink>
 
-          <NavLink  to="/dashboard" className={({isActive}: { isActive: boolean; isPending: boolean;}|any)=>isActive?'active':''}>Dashboard</NavLink>
-          <NavLink to="/grid"  className={({isActive}: { isActive: boolean; isPending: boolean;}|any)=>isActive?'active':''}>Custom Grid</NavLink>
-          <NavLink to="/world"  className={({isActive}: { isActive: boolean; isPending: boolean;}|any)=>isActive?'active':''}>WorldInfo</NavLink>
+            <NavLink
+              to="/shopping"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Online Shopping
+            </NavLink>
 
-          <NavLink to="/shopping"  className={({isActive}: { isActive: boolean; isPending: boolean;}|any)=>isActive?'active':''}>Online Shopping</NavLink>
-          
-          <NavLink to="/todo"  className={({isActive}: { isActive: boolean; isPending: boolean;}|any)=>isActive?'active':''}>ToDo</NavLink>
-          <NavLink to="/formhandling"  className={({isActive}: { isActive: boolean; isPending: boolean;}|any)=>isActive?'active':''}>Form Handling</NavLink>
-         
+            <NavLink
+              to="/todo"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              ToDo
+            </NavLink>
+            <NavLink
+              to="/formhandling"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Form Handling
+            </NavLink>
           </div>
-      </nav>
-    </header>
+        </nav>
+        </div>
+      </header>
+      
+        {
+          menuOpen && <div onClick={hideMenu} className={hamburgerMenu}>
+        <ul>
+          <li>
+            <NavLink
+              to="/dashboard"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/grid"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Custom Grid
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/world"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              WorldInfo
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/shopping"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Online Shopping
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/todo"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              ToDo
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/formhandling"
+              className={({
+                isActive,
+              }: { isActive: boolean; isPending: boolean } | any) =>
+                isActive ? 'active' : ''
+              }
+            >
+              Form Handling
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+}
+    </Fragment>
   )
-} 
+}
+
+const { yellow, hamburger, hamburgerMenu,myNav, mainNav } = styles
 
 export default Header
