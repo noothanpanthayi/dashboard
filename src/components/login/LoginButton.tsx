af9f5099dd47db1a698bdbae584ba4835e776fa0
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-02 09:06:10 -0600

    updates

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -8,14 +8,24 @@ const LoginButton = () => {
   return <>
   {
     !isAuthenticated && <>
+    <div className={container}>
+    <div >
         <button className={styles.button} onClick={()=>loginWithRedirect()}>
             Login with Auth0
         </button>
+    </div>
+    <div>
+    <span>Auth0</span>, utilizing the secure <span>OAuth</span> authorization protocol, 
+    allows you to sign up or log in seamlessly with your existing <span>Google, Microsoft, LinkedIn, or GitHub</span> accounts without sharing your credentials with the application.
+    </div>
+    </div>
         </>
 }
 
 
     </>
 }
+
+const {container}=styles;
 
 export default LoginButton
