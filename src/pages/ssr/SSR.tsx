57e14c43152003d56a9eca937160df6de0506b98
noothanpanthayi <noothankrishnan@outlook.com>
2024-12-12 16:56:58 -0600

    aws update

--- src/pages/ssr/SSR.tsx
+++ src/pages/ssr/SSR.tsx
@@ -3,7 +3,7 @@ import styles from './ssr.module.css';
 const SSR = () => {
   return <>
   <div className={styles.container}>
-        <iframe id="myIframe" className={styles.iframe} src="https://noothankrishnan.vercel.app/products" title="description"></iframe>
+        <iframe id="myIframe" className={styles.iframe} src="https://main.d3ce7oqr41spe3.amplifyapp.com/products" title="description"></iframe>
         </div>
         </>
 }
