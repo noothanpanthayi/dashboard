93472837a111ba1349b235496c0fe2e1447b45e9
noothanpanthayi <noothankrishnan@outlook.com>
2025-02-06 13:19:19 -0600

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { HashRouter } from 'react-router-dom'
 import Header from './components/header/header'
 import AppRouter from './Router'
 import Footer from './components/footer/footer'
-import { createContext, useEffect, useMemo, useState } from 'react';
-import LoginButton from './components/login/LoginButton'
-import LogoutButton from './components/login/LogoutButton'
-import { useAuth0 } from '@auth0/auth0-react'
+import { createContext, useMemo, useState } from 'react';
+// import LoginButton from './components/login/LoginButton'
+// import LogoutButton from './components/login/LogoutButton'
+// import { useAuth0 } from '@auth0/auth0-react'
 
 
 export const ScreenMode = createContext('')
@@ -14,7 +14,7 @@ export const ScreenMode = createContext('')
 const {light, dark}=styles;
 
 function App() {
-const {loginWithRedirect, isAuthenticated}=useAuth0();
+// const {loginWithRedirect, isAuthenticated}=useAuth0();
 
   const [state, setState] = useState({
     screenmode: 'light'
@@ -29,11 +29,11 @@ const {loginWithRedirect, isAuthenticated}=useAuth0();
  
   return (
     <>
-    {
+    {/* {
      !isAuthenticated && <main className={styles.main}>
       <LoginButton/>
      </main>
-    }
+    } */}
       {
         
     <ScreenMode.Provider value={value}>
@@ -41,9 +41,10 @@ const {loginWithRedirect, isAuthenticated}=useAuth0();
       <div className={state.screenmode==='light'?light:dark}>
         <HashRouter>
           <Header />
-{
+          <AppRouter />
+{/* {
        isAuthenticated && <AppRouter />
-}
+} */}
           
 
           <Footer />
